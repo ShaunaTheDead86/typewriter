1a8088d796819f83c3660aa509a4105422f2d646
Shauna Empringham <shaunathedead86@gmail.com>
2022-01-18 06:41:11 -0500

    updating typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,12 @@
 const sentence = "hello there from lighthouse labs";
+let delay = 0;
 
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 1000);
-}
+setTimeout(() => {
+  for (const char of sentence) {
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, delay);
+    delay += 500; // 50 ms is too fast, it all appears at once to me so I used 500 ms
+  }
+},1000);
+  
